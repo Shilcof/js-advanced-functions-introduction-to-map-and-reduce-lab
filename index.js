282b74e77e396db0a89c052ffaaeb7f255070b24
Shilcof <robert.shilcof@gmail.com>
2021-02-02 11:02:40 +0000

    commit tree 102569a93ff204658660d96b979b338ef1d7882b

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
 // Your code here
+
+function mapToNegativize(array) {
+    const output = array.slice();
+    for (let i = 0; i < output.length; i++) {
+        output[i] *= -1;
+    };
+    return output
+}
+
+function mapToNoChange(array) {
+    const output = array.slice();
+    return output;
+}
+
+function mapToDouble(array) {
+    const output = array.slice();
+    for (let i = 0; i < output.length; i++) {
+        output[i] *= 2;
+    };
+    return output
+}
+
+function mapToSquare(array) {
+    const output = array.slice();
+    for (let i = 0; i < output.length; i++) {
+        output[i] **= 2;
+    };
+    return output
+}
+
+// my own reduce-like methods
+function reduceToTotal(array, start=0) {
+    let output = start;
+    for (let i = 0; i < array.length; i++) {
+        output += array[i];
+    };
+    return output
+}
+
+function reduceToAllTrue(array) {
+    for (let i = 0; i < array.length; i++) {
+        if(!array[i]) {
+            return false;
+        }
+    };
+    return true;
+}
+
+function reduceToAnyTrue(array) {
+    for (let i = 0; i < array.length; i++) {
+        if(!!array[i]) {
+            return true;
+        }
+    };
+    return false;
+}
+
